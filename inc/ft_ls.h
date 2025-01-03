de36277ce81dcd3402262ec5cac7a798ff99426c
aalvarez <aingeru.develop@gmail.com>
2025-01-03 15:51:41 +0100

    feat(project setup) ✨: initialize ft_ls project structure.
    - Add main program file (`ft_ls.c`) with basic implementation.
    - Include header file (`ft_ls.h`).
    - Create `makefile` with build, clean, and submodule management rules.
    
    Signed-off-by: aalvarez <aingeru.develop@gmail.com>

---
+++ inc/ft_ls.h
@@ -0,0 +1,8 @@
+//
+// Created by aalvarez on 3/01/25.
+//
+
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#endif //FT_LS_H
---
+++ src/ft_ls.c
@@ -0,0 +1,13 @@
+//
+// Created by aalvarez on 3/01/25.
+//
+
+#include "ft_ls.h"
+
+#include <stdlib.h>
+
+int main(const int argc, const char **argv) {
+    int status = EXIT_SUCCESS;
+
+    return status;
+}
